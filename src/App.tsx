962baf4f75c88205cb22a6cf2e55cb5c8c702ce9
Sahil <thisissahil.2002@gmail.com>
2025-11-15 16:44:56 +0530

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import { Dashboard } from './components/Dashboard';
 import { mockPortfolioData } from './data/mockData';
 import { parseExcelFile } from './utils/excelParser';
